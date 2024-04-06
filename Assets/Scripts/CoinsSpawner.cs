fb6b84a07b5db1d73b2821174c5ae0defdb9e8f2
Kovalev1993 <Air1618@gmail.com>
2024-04-06 12:14:52 +0300

    Task is done

--- Assets/Scripts/CoinsSpawner.cs
+++ Assets/Scripts/CoinsSpawner.cs
@@ -1,25 +1,57 @@
+using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.XR.ARFoundation;
 
 public class CoinsSpawner : MonoBehaviour
 {
+    [SerializeField] private ARPlaneManager _planeManager;
     [SerializeField] private GameObject _coinPrefab;
     [SerializeField] private float _spawnRadius;
     [SerializeField] private int _coinsQuantity;
     [SerializeField] private CoinsCounter _coinsCounter;
 
+    private List<Transform> _coins = new();
+
     private void Start()
     {
-        SpawnCoins();
+        _planeManager.planesChanged += SpawnCoins;
+        _planeManager.planesChanged += UpdateCoins;
     }
 
-    private void SpawnCoins()
+    private void SpawnCoins(ARPlanesChangedEventArgs changes)
     {
-        for (var i = 0; i < _coinsQuantity; i++)
+        if (0 < changes.added.Count)
         {
-            var randomInCircle = Random.insideUnitCircle;
-            var randomPosition = _spawnRadius * new Vector3(randomInCircle.x, 0, randomInCircle.y);
-            var coin = Instantiate(_coinPrefab, randomPosition, Quaternion.identity).GetComponent<Coin>();
-            coin.SetCoinsCounter(_coinsCounter);
+            var plane = changes.added[0];
+            for (var i = 0; i < _coinsQuantity; i++)
+            {
+                var randomInCircle = Random.insideUnitCircle;
+                var randomPosition = _spawnRadius * new Vector3(randomInCircle.x, plane.transform.position.y, randomInCircle.y);
+                var coin = Instantiate(_coinPrefab, randomPosition, Quaternion.identity).GetComponent<Coin>();
+                coin.SetCoinsCounter(_coinsCounter);
+                _coins.Add(coin.transform);
+            }
+            _planeManager.planesChanged -= SpawnCoins;
         }
+    }
+
+    private void UpdateCoins(ARPlanesChangedEventArgs changes)
+    {
+        if (0 < changes.updated.Count)
+        {
+            var plane = changes.updated[0];
+            foreach (var coin in _coins)
+            {
+                coin.position = new Vector3(coin.position.x, plane.transform.position.y, coin.position.z);
+            }
+            _planeManager.planesChanged -= SpawnCoins;
+        }
+    }
+
+    private void OnDestroy()
+    {
+        _planeManager.planesChanged -= SpawnCoins;
+        _planeManager.planesChanged -= UpdateCoins;
     }
 }
