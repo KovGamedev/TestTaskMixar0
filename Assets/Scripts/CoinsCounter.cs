45e30ac560abdc994a4d9ba3e6d4e18af7c23c9c
Kovalev1993 <Air1618@gmail.com>
2024-04-06 11:39:16 +0300

    Coins sound

--- Assets/Scripts/CoinsCounter.cs
+++ Assets/Scripts/CoinsCounter.cs
@@ -1,15 +1,24 @@
 using TMPro;
 using UnityEngine;
+using UnityEngine.XR.ARFoundation;
 
+[RequireComponent(typeof(AudioSource))]
 public class CoinsCounter : MonoBehaviour
 {
     [SerializeField] private TextMeshProUGUI _coinsCounter;
 
+    private AudioSource _audioSource;
     private int _points = 0;
 
     public void AddPoint()
     {
         _points++;
         _coinsCounter.text = _points.ToString();
+        _audioSource.Play();
+    }
+
+    private void Awake()
+    {
+        _audioSource = GetComponent<AudioSource>();
     }
 }
